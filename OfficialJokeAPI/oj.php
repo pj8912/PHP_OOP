571fc2a1202d961116f387510fe8c339517dfe32
John Pinto <59218902+pj8912@users.noreply.github.com>
2021-09-01 08:50:44 +0530

    Update oj.php

--- OfficialJokeAPI/oj.php
+++ OfficialJokeAPI/oj.php
@@ -1,26 +1,4 @@
 <?php
-
-
-
-//    $curl = curl_init();
-//     curl_setopt($curl, CURLOPT_GET,  1);
-
-//    curl_setopt($curl, CURLOPT_URL, $url);
-//    curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-
-//       'Content-Type: application/json',
-//    ));
-
-
-//    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-//    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-//    // EXECUTE:
-//    $result = curl_exec($curl);
-//    if(!$result){die("Connection Failure");}
-//    curl_close($curl);
-//    return $result;
-
-
 $handle = curl_init();
 $url = "https://official-joke-api.appspot.com/jokes/random";
 curl_setopt($handle, CURLOPT_URL, $url);
