071cc19f129078c243e869f97c785329429655f9
pj8912 <jpintofun2b@gmail.com>
2022-02-01 00:11:52 +0530

    chaining methods

---
+++ chain.php
@@ -0,0 +1,18 @@
+<?php
+
+class User{
+	public $fname;
+	public function register(){
+		echo $this->fname." registered";
+		return $this;
+	}
+	public function mail(){
+		echo " mailed";
+	}
+}
+$user = new User();
+$user->fname ="john";
+$user->register()->mail();
+
+?>
+
