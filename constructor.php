330ffcd8be5c1164cc8888a83e7a12843651123b
pj8912 <jpintofun2b@gmail.com>
2022-01-31 22:09:27 +0530

    second file

---
+++ constructor.php
@@ -0,0 +1,17 @@
+<?php
+
+class User{
+	private $uname;
+	public function __construct($name = null){
+		$this->uname = $name;
+	}
+	public function getname():string{ 
+		return $this->uname;
+	}
+}
+
+$user = new User("john pinto");
+echo $user->getname();
+
+?>
+
