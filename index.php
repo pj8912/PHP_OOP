c879782b233e87eecb1452258d7d3a785e1cb8a2
pj8912 <jpintofun2b@gmail.com>
2022-02-04 19:05:24 +0530

    intial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title> Essentials of OOP in PHP</title>
+</head>
+
+<body>
+
+<h1 align="center"> Essentials of OOP in PHP </h1>
+1.Chaining methods
+2.access modifiers
+3.set and get methods
+4.magic methods
+5.abstract classes
+6.Interface - The next level of abstraction
+
